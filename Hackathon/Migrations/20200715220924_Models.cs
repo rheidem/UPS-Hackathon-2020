2c6866ace65eba5862fb2e67944134e5058b1cec
Heidema Ryan (ZPR9QJJ) <ZPR9QJJ@aad.ups.com>
2020-07-15 18:11:39 -0400

    updated database with new models

---
+++ Hackathon/Migrations/20200715220924_Models.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hackathon.Migrations
+{
+    public partial class Models : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    NumPoints = table.Column<int>(nullable: false),
+                    QuestionType = table.Column<int>(nullable: false),
+                    QuestionText = table.Column<string>(nullable: false),
+                    QuestionCode = table.Column<string>(nullable: true),
+                    Correct_MCAnswer = table.Column<string>(nullable: true),
+                    MCAnswers_JSON = table.Column<string>(nullable: true),
+                    Tags_JSON = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    TotalPoints = table.Column<int>(nullable: false),
+                    RelatedJob = table.Column<string>(nullable: false),
+                    Questions_JSON = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    UserType = table.Column<int>(nullable: false),
+                    Completed_Quizzes_JSON = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
